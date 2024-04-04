0652dd693d65f601631a57cf61f59f357f1c8b3f
Deepak <mjsachin78@gmail.com>
2024-04-04 15:07:03 +0530

    working in jwt

---
+++ apitest/jwtauth/jwtauth/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from api import views
+from rest_framework_simplejwt.views import TokenRefreshView,TokenObtainPairView,TokenVerifyView
+
+routes = DefaultRouter()
+
+routes.register('api',views.StudentModelViewSet, basename="student")
+
+urlpatterns = [
+    path('gettoken/',TokenObtainPairView.as_view(),name="tokenobtain"),
+    path('refreshtoken/',TokenRefreshView.as_view(),name="tokenrefresh"),
+    path('verifyview',TokenVerifyView.as_view(),name="verifyuser"), # optional
+    path('admin/', admin.site.urls),
+    path('',include(routes.urls)),
+]
